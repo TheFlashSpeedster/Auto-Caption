62208fd6e2efde8a69601ff6807e5028aacdd56c
The Flash <atulk5137@gmail.com>
2022-11-12 11:35:12 +0530

    Test config

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "AvishkarPatil")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5569794009:AAHLwWzY6-gU20BDjXdIZe7uaG9QXzsgiFs")
+      API_ID = int(os.environ.get("APP_ID", 28775362))
+      API_HASH = os.environ.get("API_HASH", "773aa8651092039c392a183316fb9e69")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "Join @Ace_ML")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "BOTTOM")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "FlashSpeedster")
